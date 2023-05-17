0750e9a7ece9d353d2c976275bedc419d97c1665
haiiliin <hailin.wang@connect.polyu.hk>
2023-05-17 19:42:09 +0800

    Add run command lin application

--- abqcy/cli.py
+++ abqcy/cli.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import os
 import re
+import shutil
 from pathlib import Path
 
 from abqpy.cli import abaqus
@@ -11,9 +12,64 @@ class AbqcyCLI:
     """The ``abqcy`` command-line interface."""
 
     def compile(self, script: str):
-        """Compile a Cython script to an Abaqus user subroutine as an object file."""
+        """Compile a Cython script to an Abaqus user subroutine as an object file.
+
+        Parameters
+        ----------
+        script : str
+            The path to the Cython script to compile.
+        """
         os.system(f"cython -a {script}")
         compiled = Path(script).with_suffix(".c")
         replaced = re.sub("__PYX_EXTERN_C void ", 'extern "C" void ', compiled.read_text())
         compiled.write_text(replaced)
         abaqus.abaqus("make", library=str(compiled))
+
+    def run(self, input: str, user: str, job: str = None, wd: str = None, script: str = None, **kwargs):
+        """Run Abaqus jobs.
+
+        Parameters
+        ----------
+        input : str
+            The path to the input file.
+        user : str
+            The name of the user subroutine.
+        job : str, optional
+            The name of the job, by default the current directory name.
+        wd : str, optional
+            The working directory, by default the directory of the input file.
+        script : str, optional
+            The Python script to run after finishing the job to post-process the results.
+        """
+        # Prepare the working directory
+        wd = Path(wd or ".").resolve()
+        job = job or Path(input).stem
+        if not wd.exists():
+            wd.mkdir(parents=True)
+        if not (wd / Path(input).name).exists():
+            shutil.copy(input, wd)
+        if not (wd / Path(user).name).exists():
+            shutil.copy(user, wd)
+        if script and not (wd / Path(script).name).exists():
+            shutil.copy(script, wd)
+        os.chdir(wd)
+
+        # Compile the user subroutine if it is a Cython/Pure Python script
+        if user.endswith((".pyx", ".py")):
+            self.compile(Path(user).name)
+            compiled = False
+            for file in wd.glob(f"{Path(user).stem}-*.obj"):
+                user = file
+                compiled = True
+                break
+            assert compiled, f"Failed to compile {user} to an object file."
+
+        # Run the job
+        inp = Path(input).stem
+        user = Path(user).stem
+        abaqus.abaqus("", job=job, input=inp, user=user, interactive=True, **kwargs)
+
+        # Run the post-processing script
+        if script:
+            script = Path(script).name
+            abaqus.cae(script, gui=False)
--- tests/run.bat
+++ tests/run.bat
@@ -1,19 +1,5 @@
 REM Change directory to the directory of this file
 cd /D "%~dp0"
 
-REM Compile the subroutines
-if not exist subs mkdir subs
-cd subs
-python3.11 ..\..\abqcy\__main__.py compile ..\..\abqcy\subroutines\elastic.pyx
-cd ..
-
-REM Run the job
-set inp=column.inp
-set sub=elastic-std.obj
-set job=Job-column-elastic
-if not exist jobs\%job% mkdir jobs\%job%
-echo F|xcopy subs\%sub% jobs\%job%\%sub% /Y
-echo F|xcopy inputs\%inp% jobs\%job%\%inp% /Y
-cd jobs\%job%
-abaqus job=%job% input=%inp% user=%sub% interactive
-cd ..\..
+pip install ..
+abqcy run --job=Job-column-elastic --input=inputs/column.inp --user=../abqcy/subroutines/elastic.pyx --wd=jobs/Job-column-elastic
