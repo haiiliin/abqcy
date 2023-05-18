d227325ed31a00e3ae393817af8ec64ad793309c
haiiliin <hailin.wang@connect.polyu.hk>
2023-05-18 20:24:09 +0800

    Add tests

---
+++ tests/conftest.py
@@ -0,0 +1,19 @@
+"""
+conftest.py: sharing fixtures across multiple files. https://docs.pytest.org/en/7.1.x/reference/fixtures.html#conftest-py-sharing-fixtures-across-multiple-files.
+"""
+import os
+
+import pytest
+
+
+def pytest_configure(config: pytest.Config) -> None:
+    """Allow plugins and conftest files to perform initial configuration.
+
+    This hook is called for every plugin and initial conftest file after command line options have been parsed.
+
+    After that, the hook is called for other conftest files as they are imported.
+
+    Args:
+        config (pytest.Config): The pytest config object.
+    """
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
---
+++ tests/test_models.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import pytest
+from matplotlib import pyplot as plt
+
+
+@pytest.mark.parametrize(
+    argnames="data_path, xcol, ycol, xlabel, ylabel",
+    argvalues=[("outputs/elastic/U3.csv", "time", "U3", "Time (s)", "Displacement (m)")],
+    ids=["elastic"],
+)
+@pytest.mark.mpl_image_compare
+def test_model(data_path: str, xcol: str, ycol: str, xlabel: str, ylabel: str):
+    data = pd.read_csv(data_path)
+    x, y = data[xcol], data[ycol]
+    fig, ax = plt.subplots()
+    ax.plot(x, y)
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    ax.grid()
+    return fig
