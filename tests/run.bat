655a480550375b856c2117c76debdda13350d661
haiiliin <hailin.wang@connect.polyu.hk>
2023-05-19 16:20:49 +0800

    Use elastic.pyx in run.bat

--- tests/run.bat
+++ tests/run.bat
@@ -2,5 +2,5 @@ REM Change directory to the directory of this file
 cd /D "%~dp0"
 
 pip install ..
-abqcy run --job=Job-element-elastic --model=models/element.py --user=../examples/elastic.py --output=jobs/Job-element-elastic --post=models/element-output.py --visualization=models/element-visualization.py
+abqcy run --job=Job-element-elastic --model=models/element.py --user=../examples/elastic.pyx --output=jobs/Job-element-elastic --post=models/element-output.py --visualization=models/element-visualization.py
 xcopy jobs\Job-element-elastic\U3.* outputs\element\elastic\ /C /S /D /Y /I
